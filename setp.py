ae1f4f7e095bab6198428b8d3cfcff41640c9083
Navdeep6 <70259179+Navdeep6@users.noreply.github.com>
2021-01-06 18:34:49 +0400

    Update setp.py

--- setp.py
+++ setp.py
@@ -8,7 +8,7 @@ setuptools.setup(name="navdeepPackage",
                  description="This package shows if a number is prime,odd or even",
                  long_description=long_desc,
                  long_description_content_type="tent/markdown",
-                 url="",
+                 url="https://github.com/Navdeep6/navdeepPackage",
                  packages=setuptools.find_packages(),
                  classifiers=["programing language::python::3","operating system::os independent,],
                  python_requiers='>=3.6',)
